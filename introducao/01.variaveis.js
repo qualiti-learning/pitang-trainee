f18ad4c962f0a760e247f5163ab96bae2e9eefee
Keven Leone <keven.leone@liferay.com>
2022-01-12 22:06:19 -0300

    Aula 01

---
+++ introducao/01.variaveis.js
@@ -0,0 +1,36 @@
+const trabalhando = true;
+const nome = null;
+const abc;
+
+const aluno = {
+  nome: '"Keven"',
+  idade: 25.23,
+};
+
+const alunos = [
+  "Keven",
+  "Eduarda",
+  "Iago",
+  1,
+  2,
+  true,
+  false,
+  {},
+  [],
+  new Date(),
+];
+
+let idade = 25;
+
+idade /= 10;
+
+let x = 3;
+let y = 4;
+
+x = 6;
+
+const aluno1 = "keven\n123" + aluno2 + aluno3;
+const aluno2 = "leone";
+const aluno3 = `${aluno1} ${aluno2} testando 123.... ${x}`;
+
+console.log(aluno3);
