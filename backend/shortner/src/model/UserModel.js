684a994bb6fe3f5057d305724d02cdc6c977bc69
Keven Leone <keven.leone@liferay.com>
2022-02-14 20:07:49 -0300

    Code

--- backend/shortner/src/model/UserModel.js
+++ backend/shortner/src/model/UserModel.js
@@ -1,5 +1,9 @@
 import crypto from "crypto";
 
+// Usar Model do mongoose para usuário:
+
+// Name, Email, Role: Administrator / User, Password, Created At, Modified At, Phones: 123, 123
+
 export const users = [
   {
     id: crypto.randomUUID(),
