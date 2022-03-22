327229592c1b85ac842dcded3dc98ffdf841d406
Keven Leone <keven.santos.sz@gmail.com>
2022-03-22 19:16:54 -0300

    React

--- packages/movie-frontend/src/App.js
+++ packages/movie-frontend/src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card, FormControl, Button, Row, Col } from 'react-bootstrap'
+import { useState } from 'react'
 
-function App() {
+const CardWrapper = ({ children, title }) => (
+  <Card>
+    <Card.Header>
+      <Card.Title>
+        {title}
+      </Card.Title>
+    </Card.Header>
+
+    <Card.Body>
+      {children}
+    </Card.Body>
+  </Card>
+)
+
+const users = [
+  {
+    name: "Keven Leone",
+    login: "kevenleone",
+    bio: "Frontend-Engineer",
+  },
+  {
+    name: "Zeno Rocha",
+    login: "zenorocha",
+    bio: "Zeno Rocha",
+  },
+  {
+    name: "Liferay",
+    login: "liferay",
+    bio: "Open Source",
+  },
+  {
+    name: "Linus Torvalds",
+    login: "torvalds",
+    bio: "Linux",
+  }
+]
+
+const RecentUser = ({ login, name }) => (
+  <>
+    <img
+      width={40}
+      height={40}
+      src={`https://github.com/${login}.png`} alt="" />
+    <span> {name}</span>
+  </>
+)
+
+async function customFetch(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+
+  return data
+}
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [user, setUser] = useState();
+
+  const onSearchUser = async () => {
+    const user = await customFetch(`https://api.github.com/users/${username}`)
+    const repos = await customFetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+
+    setUser({
+      ...user,
+      repos
+    })
+  }
+
+  const onChange = (event) => {
+    setUsername(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editado <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <Row className="mb-4">
+        <Col>
+          <FormControl placeholder="Type a Github Username" onChange={onChange} />
+        </Col>
+
+        <Col>
+          <Button onClick={onSearchUser} variant='primary'>Search User</Button>
+        </Col>
+      </Row>
+
+      <CardWrapper title="Recent Users">
+        {users.map((user) =>
+          <RecentUser
+            key={user.login}
+            name={user.name}
+            login={user.login}
+            bio={user.bio} />
+        )}
+      </CardWrapper>
+
+      {user && <CardWrapper title="Github User">
+          <img
+            className='rounded-circle'
+            width={240}
+            height={240}
+            src={`https://github.com/${user.login}.png`}
+            alt=""
+          />
+
+          <h1>{user.name}</h1>
+          <strong>{user.login}</strong>
+          <p>{user.bio}</p>
+
+          <ul>
+            {user.repos.map((repo) => (
+              <li key={repo.id}>{repo.name}</li>
+            ))}
+          </ul>
+      </CardWrapper>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
