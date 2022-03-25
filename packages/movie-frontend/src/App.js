919ed6787000addcfc745fc4e5e7bc988ce34356
Keven Leone <keven.leone@liferay.com>
2022-03-24 21:58:45 -0300

    SpaceX Demo

--- packages/movie-frontend/src/App.js
+++ packages/movie-frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.scss";
 
-function App() {
+const Launch = ({
+  description,
+  img,
+  launchDate = "Data não especificada",
+  title,
+}) => (
+  <div className="launch">
+    <img alt="Thaicon 6 Launch logo" src={img} />
+
+    <div className="content">
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <span>{launchDate}</span>
+    </div>
+  </div>
+);
+
+const App = () => {
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    async function getLaunches() {
+      const response = await fetch("https://api.spacexdata.com/v4/launches");
+      const data = await response.json();
+
+      setLaunches(data.slice(0, 30));
+    }
+
+    getLaunches();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {launches.map((launch) => (
+        <Launch
+          description={launch.details}
+          img={launch.links.patch.large}
+          launchDate={launch.date_utc}
+          title={launch.name}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
