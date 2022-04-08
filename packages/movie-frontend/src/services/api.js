57680d1d003af69c05dec4c751e67330bf89f444
Keven Leone <keven.leone@liferay.com>
2022-04-08 20:18:09 -0300

    Add ENV

--- packages/movie-frontend/src/services/api.js
+++ packages/movie-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAxios = axios.create({
-  baseURL: "http://localhost:3333/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3333/api",
 });
 
 export default baseAxios;
